d33c0d27653b5e157479e9640e6230f8f16ffedb
Amy Chen <amychen.dev@gmail.com>
2020-08-19 21:52:39 -0700

    added server setup

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const path = require('path');
+const http = require('http')
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when a client connects --> To a single client
+io.on('connection', socket => {
+    // console.log('New WS Connection...');
+
+    // Welcome new user
+    socket.emit('message', 'Welcome to Le Chat!');
+
+    // Announce when a new user connects --> To all clients EXCEPT client that is connecting
+    socket.broadcast.emit('message', 'A user has joined the chat room');
+
+    // Run when a client disconnects --> To ALL clients
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat room');
+    });
+
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
