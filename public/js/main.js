898cb3c2e888736822d85290dbf35d85b41d8a90
Amy Chen <amychen.dev@gmail.com>
2020-08-19 23:31:50 -0700

    added message output and append to the DOM

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+// Emit form inputs (e.g. sending a message) to server and add to the DOM
+const chatForm = document.getElementById('chat-form');
+
+// Connect to socket.io in server
+const socket = io();
+
+// Show message from the server associated with each action (e.g. welcome user, new user joined, user left, etc.)
+socket.on('message', message => {
+    // console.log(message);
+
+    // Output Options: templating engines (handlebars.js, mustache), React
+    outputMessage(message);
+});
+
+// Submit a message
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevent page refresh
+
+    // Get message text
+    const msg = e.target.elements.msg.value;
+    // console.log(msg);
+
+    // Emit message to the server
+    socket.emit('chatMessage', msg);
+});
+
+// Output message to the DOM
+function outputMessage(message) {
+    // Create a new div for the message
+    const div = document.createElement('div');
+
+    // Add the message to the new div
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Amy <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+
+    // Append the message to the DOM
+    document.querySelector('.chat-messages').appendChild(div);
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const http = require('http')
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
@@ -10,23 +10,31 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Run when a client connects --> To a single client
+// Run when a user connects
 io.on('connection', socket => {
     // console.log('New WS Connection...');
 
-    // Welcome new user
+    // Welcome new user --> Emits to a SINGLE client
     socket.emit('message', 'Welcome to Le Chat!');
 
-    // Announce when a new user connects --> To all clients EXCEPT client that is connecting
-    socket.broadcast.emit('message', 'A user has joined the chat room');
+    // Announce when a new user connects --> Emits to all clients EXCEPT the client that is connecting
+    socket.broadcast.emit('message', 'A user has joined the chat');
 
-    // Run when a client disconnects --> To ALL clients
+    // Run when a user disconnects (needs to be inside socket 'io.on('connection') --> Emits to ALL clients
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat room');
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // Listen for chatMessage & catch the emitted message from the front end (main.js)
+    socket.on('chatMessage', (msg) => {
+        // console.log(msg);
+        
+        // Emit the message back to the client
+        io.emit('message', msg)
     });
 
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
